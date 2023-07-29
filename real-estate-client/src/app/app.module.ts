eacc4f0334ef158c7a67b674790a594bc0d37d6b
Raghuvardhan Karanam <karanamraghuvardhan12@gmail.com>
2023-07-29 19:52:56 +0530

    add listing component

--- real-estate-client/src/app/app.module.ts
+++ real-estate-client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ListingComponent } from './listing/listing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ListingComponent
   ],
   imports: [
     BrowserModule,
---
+++ real-estate-client/src/app/listing/listing.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Property } from '../property';
+
+@Component({
+  selector: 'app-listing',
+  templateUrl: './listing.component.html',
+  styleUrls: ['./listing.component.css']
+})
+export class ListingComponent implements OnInit {
+  @Input() property: Property;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
